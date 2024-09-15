377af067031618492258fde08f9d4f2d9e282a19
Quốc Phạm <zzqucppzz87>
2024-09-15 19:18:53 +0700

    update for request open

--- locker/Locker.py
+++ locker/Locker.py
@@ -5,7 +5,14 @@ from enum import Enum
 
 class REQUEST(Enum):
     OPEN = "open"
+    CLOSE = "close"
     PRINT_QR = "print_qr"
+
+class UPDATE(Enum):
+    OPENING = "opening"
+    CLOSING = "closing"
+    OCCUPIED = "occupied"
+    EMPTY = "empty"
 
 class Locker(mqtt.Client):
     id: int
@@ -39,27 +46,41 @@ class Locker(mqtt.Client):
     
     def update_status(self, cell_id, status: CELL_STATUS):
         self.cells[cell_id].status = status
-        self.publish(f"locker/{self.id}/cell/{cell_id}", {
-            "status": status.value
-        })
+        # self.publish(f"locker/{self.id}/cell/{cell_id}", f'"status": "{status.value}"', 0)
+
+    def update_status_door(self, cell_id, status: CELL_STATUS):
+        self.cells[cell_id].status_door = status
 
     def on_connect(self, client, userdata, flags, reason_code, properties):
         print(f"Connected with result code {reason_code}")
         # self.publish(f"locker/{self.id}/cell/2", "1")
 
     def on_message(self, client, userdata, msg):
-        topic = msg.topic
-        body = json.loads(msg.payload.decode("utf-8"))
-        cell_id = topic.split("/")[-1]
-        # Format body to json
-        if "request" in body:
-            request = body["request"]
-            if request == REQUEST.OPEN.value:
-                self.open_cell(cell_id)
-            elif request == REQUEST.PRINT_QR.value:
-                self.print_QR_code(body["order_id"], body["OTP"])
+        try:
+            topic = msg.topic
+            body = json.loads(msg.payload.decode("utf-8"))
+            cell_id = topic.split("/")[-1]
+            # Format body to json
+            if "request" in body:
+                request = body["request"]
+                if request == REQUEST.OPEN.value:
+                    self.open_cell(cell_id)
+                elif request == REQUEST.PRINT_QR.value:
+                    self.print_QR_code(body["order_id"], body["OTP"])
+                elif request == REQUEST.CLOSE.value:
+                    self.close_cell(cell_id)
+            elif "update" in body:
+                update = body["update"]
+                if update == UPDATE.OPENING.value:
+                    self.update_status_door(cell_id, UPDATE.OPENING)
+                elif update == UPDATE.CLOSING.value:
+                    self.update_status_door(cell_id, UPDATE.CLOSING)
+            else:
+                request = "None"
 
-        print(f"Received message: {cell_id} {request}")
+            print(f"Received message: {cell_id} {request}")
+        except json.JSONDecodeError as e:
+            print(f"Failed to decode JSON: {e}")
 
     def on_subscribe(self, client, userdata, mid, reason_code, properties):
         print(f"Locker subscribed: {self.id}")
@@ -69,20 +90,23 @@ class Locker(mqtt.Client):
             raise Exception("Host and port must be set")
         super().connect(self.host, self.port, keepalive)
         self.subscribe(f"locker/{self.id}/cell/#")
+        self.subscribe("rpi/locker/#")
 
     def open_cell(self, cell_id):
-        cell = self.get_cell(cell_id)
+        cell = self.get_cell(int(cell_id))
+        self.publish(f"rpi/locker/{cell_id}", '{"cell":"on"}', 0)
         if cell.status == CELL_STATUS.OCCUPIED:
-            self.update_status(cell_id, False)
+            self.update_status(int(cell_id), False) #False?
         else:
             print("Cell is empty")
     
     def close_cell(self, cell_id):
-        cell = self.get_cell(cell_id)
-        if cell.occupied:
+        cell = self.get_cell(int(cell_id))
+        self.publish(f"rpi/locker/{cell_id}", '{"cell":"off"}', 0)
+        if cell.status == CELL_STATUS.OCCUPIED:
             print("Cell is occupied")
         else:
-            self.update_status(cell_id, True)
+            self.update_status(int(cell_id), True)
     
     def print_QR_code(self, order_id, OTP):
         # TODO: Implement this function
