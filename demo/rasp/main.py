967652650b2670d3c480bf1775cdeac26fde7b07
ppquoc87 <ppquoc23@gmail.com>
2024-12-09 19:00:53 +0700

    Update main.py

--- demo/rasp/main.py
+++ demo/rasp/main.py
@@ -14,23 +14,17 @@ MQTT_PORT = int(config("MQTT_PORT"))
 HTTP_HOST = config("HTTP_HOST")
 HTTP_PORT = int(config("HTTP_PORT"))
 IS_HTTPS = config("IS_HTTPS")
+
 ID = config("ID")
 if IS_HTTPS == "True":
     IS_HTTPS = True
 else:
     IS_HTTPS = False
 
-locker = Locker(2, MQTT_HOST, MQTT_PORT, app.generate_qr_code)
+locker = Locker(2, MQTT_HOST, MQTT_PORT, app.generate_qr_code, app.display_sucess)
 print(MQTT_HOST, MQTT_PORT)
 locker.connect(60)
 locker.connect_to_http(HTTP_HOST, HTTP_PORT, IS_HTTPS)
-# for i in range(1, 11):
-#     locker.add_cell(i)
-
-# message = '{"request":"open"}'
-#json_string = msg.payload.decode("utf-8").replace("'", '"').replace("False", "false").replace("True", "true").replace("None", "null")
-
-# locker.publish("locker/1/cell/5", message, 0)
 
 locker.loop_start()
 root.mainloop()    # You can directly call the function with order_id and otp5
